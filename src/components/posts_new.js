3868b074413a6badff303d933ffda095b41897fc
dustin <dandersonsoftware@outlook.com>
2018-05-16 09:13:59 -0400

    setup validation with reduxForm

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,12 +11,16 @@ class PostsNew extends Component {
           type="text"
           {...field.input}
         />
+        {field.meta.error}
       </div>
     );
   }
 
 // field is an object ... means we want all those properties to be props
 // for the input
+// Field doesn't know what to show on the screen. It only knows how to
+// interact with the data. Component prop takes in a function that should
+// return JSX. No parentheses as we are just passing a reference to a function
 
   render() {
     return (
@@ -40,12 +44,31 @@ class PostsNew extends Component {
     );
   }
 }
-// Field doesn't know what to show on the screen. It only knows how to
-// interact with the data. Component prop takes in a function that should
-// return JSX. No parentheses as we are just passing a reference to a function
+
+// values is an object that contains all the values the user has enetered
+// on the form. Function needs to create and return an errors object
+function validate(values) {
+  const errors = {};
+
+  // Validate the inputs from 'values'
+  if (!values.title || values.title.length < 3) {
+    error.title = "Title must be at least 3 characters long!";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter at least one category!";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content!";
+  }
+  // If errors is empty the form is fine to submit
+  // If errors has any properties reduxForm assumes forms is invalid
+  return errors;
+}
 
 // Unique name for form in case we had multiple forms on the same page
 // Holds true for basic forms
+// validate: validate
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
